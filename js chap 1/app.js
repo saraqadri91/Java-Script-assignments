4462e1e6906b77d4afd8e411d0602cd4647a564e
saraqadri91 <qadrisara91@gmail.com>
2024-05-06 23:17:59 +0500

    1st chap completed

---
+++ js chap 1/app.js
@@ -0,0 +1,18 @@
+
+//  question 1
+// alert("HELLO");
+
+//  question 2
+// alert("Error! please enter a valid password.")
+
+//  question 3 
+// alert("Welocome to JS land... \n Happy coding!")
+
+//  question 4
+// alert("Welocome to JS land...");
+// alert("happy coding! \n prevent this page from creating additional dialogs");
+
+//  question 5
+alert("hello i can run js through my web browser's console");
+console.log("Alert displayed successfully!");
+
