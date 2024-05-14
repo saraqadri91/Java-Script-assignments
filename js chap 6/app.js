77520430dcbc5ba254b5ed6bd44ac83490809179
saraqadri91 <qadrisara91@gmail.com>
2024-05-15 00:09:02 +0500

    done

---
+++ js chap 6/app.js
@@ -0,0 +1,93 @@
+// question # 1
+
+// document.write("Result: <br> The value of a is: 10" + "<br>" + "<br>" + "<br>");
+
+
+// var a = "10"
+// document.write("The value of ++a is " + ++a + "<br>")
+// document.write("The value of a is " + a + "<br>" + "<br>" + "<br>")
+
+// document.write("the value of a++ is " + a++ + "<br>")
+// document.write("the value of a is " + a + "<br>" + "<br>" + "<br>")
+
+// document.write("The value of --a is " + --a + "<br>")
+// document.write("The value of a is " + a + "<br>" + "<br>" + "<br>")
+
+// document.write("The value of a-- is " + a-- + "<br>")
+// document.write("The value of a is " + a + "<br>" + "<br>" + "<br>")
+
+
+
+// question # 2
+// var a = 2
+// var b = 1
+// console.log(--a)
+// console.log(--a - --b)
+// console.log(--a - --b + ++b)
+// console.log(--a - --b + ++b + b--)
+// document.write("a=" + a + "<br>")
+// document.write("b=" + b + "<br>")
+// var result = --a - --b + ++b + b--
+// document.write("Result: " + result)
+
+
+
+// question # 3
+// var userName = prompt("Please Write your name")
+// alert("Welcome " + userName)
+
+
+// question # 5
+// var userNum = prompt("Write the number for which you want the table.")
+// var result = userNum*1
+// document.write(userNum + " X " + "1 " + "= " + result + "<br>")
+// var result = userNum*2
+// document.write(userNum + " X " + "2 " + "= " + result + "<br>")
+// var result = userNum*3
+// document.write(userNum + " X " + "3 " + "= " + result + "<br>")
+// var result = userNum*4
+// document.write(userNum + " X " + "4 " + "= " + result + "<br>")
+// var result = userNum*5
+// document.write(userNum + " X " + "5 " + "= " + result + "<br>")
+// var result = userNum*6
+// document.write(userNum + " X " + "6 " + "= " + result + "<br>")
+// var result = userNum*7
+// document.write(userNum + " X " + "7 " + "= " + result + "<br>")
+// var result = userNum*8
+// document.write(userNum + " X " + "8 " + "= " + result + "<br>")
+// var result = userNum*9
+// document.write(userNum + " X " + "9 " + "= " + result + "<br>")
+// var result = userNum*10
+// document.write(userNum + " X " + "10 " + "= " + result + "<br>")
+
+// question 6
+// var subject1 = prompt("Kindly Write 1st subject")
+// var subject2 = prompt("Kindly Write 2nd subject")
+// var subject3 = prompt("Kindly Write 3rd subject")
+// var subject1Marks = prompt("Kindly Write" + subject1 + "marks","100 out of")
+// var subject2Marks = prompt("Kindly Write" + subject2 + "marks","100 out of")
+// var subject3Marks = prompt("Kindly Write" + subject3 + "marks","100 out of")
+// var totalMarks = 100
+// var res1 = subject1Marks/totalMarks*100;
+// var res2 =subject2Marks/totalMarks*100;
+// var res3 =subject3Marks/totalMarks*100;
+// document.write("<b>Subject Total Marks Obtained Marks Percentage</b><br>")
+// document.write("<pre>" + subject1 + "        " + totalMarks + "        " + subject1Marks + "               " + res1 + "%</pre>");
+// document.write("<pre>" + subject2 + "        " + totalMarks + "        " + subject2Marks + "               " + res2 + "%</pre>");
+// document.write("<pre>" + subject3 + "        " + totalMarks + "        " + subject3Marks + "               " + res3 + "%</pre>");
+
+// var totalObtainedMarks = parseInt(subject1Marks) + parseInt(subject2Marks) + parseInt(subject3Marks);
+// var finalMarks = totalMarks + totalMarks + totalMarks
+// var finalPercentage = (res1 + res2 + res3) / 3;
+// document.write("<pre>"  +"        "  + finalMarks + "        " + totalObtainedMarks + "        " + finalPercentage +"%</pre>");
+
+
+
+
+
+
+
+
+
+
+
