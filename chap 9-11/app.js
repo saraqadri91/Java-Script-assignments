68c69528ed37d882592159e4e6fc91f81b5dfedf
saraqadri91 <qadrisara91@gmail.com>
2024-05-22 00:37:21 +0500

    updated

---
+++ chap 9-11/app.js
@@ -0,0 +1,12 @@
+// question 1
+// var cityName = prompt("Enter your city name")
+// if(cityName == "karachi"){alert("Welcome to city of lights")}
+// else{}
+
+// question 2
+// var userGender = prompt("write your gender")
+// if(userGender == "male"){alert("Good Morning Sir")}
+// else{alert("Good Morning Ma'am")}
+
+// question 3
+var
