07d4820cfff9f42ecb078049806f3c1399c7d4c2
saraqadri91 <qadrisara91@gmail.com>
2024-05-28 00:45:48 +0500

    updated

--- chap 9-11/app.js
+++ chap 9-11/app.js
@@ -9,4 +9,161 @@
 // else{alert("Good Morning Ma'am")}
 
 // question 3
-var
+// var signalColour = prompt("Enter the colour of signal light")
+// if(signalColour == "red"){alert("Must stop")}
+// else if(signalColour == "yellow"){alert("Ready to move")}
+// else if(signalColour == "green"){alert("Move now")}
+// else{prompt("Enter the colour of signal light") }
+
+// question 4
+// var userFuel = prompt("Enter reamaining fuel (in lit.) ")
+// if (userFuel < "0.25"){alert("Please refill the fuel in your car")}
+// else{alert("That's great😊 Have a great journey")}
+
+// question 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");}
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// question 6
+// var subject1 = +prompt("Enter marks of subject 1")
+// var subject2 = +prompt("Enter marks of subject 2")
+// var subject3 = +prompt("Enter marks of subject 3")
+// var totalMarks = 300;
+// var obtainedMarks = subject1 + subject2 + subject3
+// var percentage = (obtainedMarks / totalMarks) * 100
+// if (percentage >= 80)
+//      { document.write("<center><b>Mark Sheet</b> <br /> <br /> <br /> <center><b>Total Marks: </center>" + totalMarks + " <br /> <br />" + "<center>Marks Obtained: </center>"+ obtainedMarks + " <br /> <br />" + "<center>Percentage:</center>" +percentage+ " <br /> <br />" + "<center>Grade: A-one</center>" + " <br /> <br />" + "<center>Remarks: Excellent</center>") }
+//   else if (percentage >= 70) 
+// { document.write("<center><b>Mark Sheet</b> <br /> <br /> <br /> <center><b>Total Marks: </center>" + totalMarks + " <br /> <br />" + "<center>Marks Obtained: </center>"+ obtainedMarks + " <br /> <br />" + "<center>Percentage:</center>" +percentage+ " <br /> <br />" + "<center>Grade: A</center>" + " <br /> <br />" + "<center>Remarks: Good</center>") }
+//   else if (percentage >= 60) 
+//   { document.write("<center><b>Mark Sheet</b> <br /> <br /> <br /> <center><b>Total Marks: </center>" + totalMarks + " <br /> <br />" + "<center>Marks Obtained: </center>"+ obtainedMarks + " <br /> <br />" + "<center>Percentage:</center>" +percentage+ " <br /> <br />" + "<center>Grade: B</center>" + " <br /> <br />" + "<center>Remarks: You need to improve</center>") }
+//   else if (percentage < 60) 
+//   { document.write("<center><b>Mark Sheet</b> <br /> <br /> <br /> <center><b>Total Marks: </center>" + totalMarks + " <br /> <br />" + "<center>Marks Obtained: </center>"+ obtainedMarks + " <br /> <br />" + "<center>Percentage:</center>" +percentage+ " <br /> <br />" + "<center>Grade: Fail</center>" + " <br /> <br />" + "<center>Remarks: Sorry</center>") }
+// else{ "Please refresh your page"}
+
+// question 7
+// var myNum = 3
+// var userNum = +prompt("Guess number between 1-10")
+// if (userNum == userNum+"1") { alert("Close enough to the correct answer") }
+// else if (myNum == userNum) { alert("Bingo! Correct answer") }
+// else { alert("Wrong answer") }
+
+// question 8
+// var userNum = +prompt("Enter your number")
+// if (userNum % 3 == 0) {
+//     alert("The number " + userNum + " is divisible by 3.")
+
+// }
+// else {
+//     alert("The number " + userNum + " is not divisible by 3.");
+// }
+
+// question 9 
+// var userNumber = +prompt("Enter a number:");
+// if (userNumber % 2 === 0) {
+//     alert("The number " + userNumber + " is even.");
+// } else {
+//     alert("The number " + userNumber + " is odd.");
+// }
+
+
+// question 10
+// var userTemp = +prompt("Enter temperature")
+// if (userTemp > 40) {alert("It is too hot outside")
+
+// }
+// else if (userTemp > 30) {alert("The Weather today is Normal")
+
+// }
+// else if (userTemp > 20) {alert("Todays Weather is cool")
+
+// }
+// else if (userTemp > 10) {alert("OMG! Todays weather is so Cool")
+
+// }
+// else{alert("OMG! Todays weather is so Cool")
+// }
+
+// question 11
+// Prompt the user to enter the first number
+// var userNum1 = +prompt("Enter 1st number");
+
+// Prompt the user to enter the operation
+// var operation = prompt("Enter the operation you want to do", "+ - * / %");
+
+// Prompt the user to enter the second number
+// var userNum2 = +prompt("Enter 2nd number");
+
+// Variable to hold the result
+// var result;
+
+// Perform the operation based on user input
+// if (operation === "+") {
+//     result = userNum1 + userNum2;
+// } else if (operation === "-") {
+//     result = userNum1 - userNum2;
+// } else if (operation === "*") {
+//     result = userNum1 * userNum2;
+// } else if (operation === "/") {
+//     // Check for division by zero
+//     if (userNum2 !== 0) {
+//         result = userNum1 / userNum2;
+//     } else {
+//         alert("Division by zero is not allowed.");
+//         result = "undefined";
+//     }
+// } else if (operation === "%") {
+//     // Check for division by zero
+//     if (userNum2 !== 0) {
+//         result = userNum1 % userNum2;
+//     } else {
+//         alert("Division by zero is not allowed.");
+//         result = "undefined";
+//     }
+// } else {
+//     alert("Invalid operation.");
+//     result = "undefined";
+// }
+
+// // Display the result to the user
+// if (result !== "undefined") {
+//     alert("The result of " + userNum1 + " " + operation + " " + userNum2 + " is: " + result);
+// }
+
+
+
---
+++ js chap 12-13/app.js
@@ -0,0 +1,58 @@
+// question 2
+// var firstNum = +prompt("Enter first integer")
+// var secNum = +prompt("Enter second integer")
+// if (firstNum > secNum) {
+//     alert(firstNum + "is greater than" + secNum)
+
+// }
+// else if (firstNum < secNum) {
+//     alert(secNum + "is greater than" + firstNum)
+
+// }
+// else {
+//     alert(secNum + "is equal to" + firstNum)
+
+// }
+
+// question 3
+// var firstNum = +prompt("Enter first number")
+// if (firstNum > 0) {
+//     alert("The number is positive")
+
+// }
+// else if (firstNum < 0) {
+//     alert("The number is negative")
+
+// }
+// else {alert("The number is zero")}
+
+// question 4
+// var vowel = prompt("Enter an alphabet");
+
+// if (vowel.length == 1 && isNaN(vowel) && (vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u")) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// question 5
+// var userPassword1 = prompt("Enter your password")
+// var userPassword2 = prompt("Enter your password again for confirmation:")
+// if (!userPassword1 || !userPassword2) {
+//     prompt("Please enter your password")
+// }
+// else if (userPassword1 == userPassword2) {
+//     alert("“Correct! The password you entered matches the original password")
+// }
+// else if (userPassword1 !== userPassword2) {
+//     alert("Incorrect password")
+// }
+
+//  question 6
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";}
+else{
+    greeting = "Good evening";
+}
